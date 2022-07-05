2e6c93a8e906775ef9ff9a8c084bdd3efdc4c53d
Mridul Dey <51965740+mriduldey@users.noreply.github.com>
2022-07-06 02:09:59 +0530

    searching basic implementation searching basic implementation, needs to be improved

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import Pagination from "./components/Pagination";
 import Store from "./utils/store";
 
 function render(data) {
-  document.querySelector("#app").innerHTML = Input({ id: "hihihi" })
+  document.querySelector("#app").innerHTML = Input({
+    id: "search-input",
+    placeholder: "Search"
+  })
     .concat(Table({ id: "table" }, data.slice(0, 10)))
     .concat(Pagination({}, data.length));
 }
@@ -17,6 +20,14 @@ getData().then((data) => {
   const store = new Store(render);
   store.array = data;
 
-  data.splice(3, 23);
-  store.array = data;
+  const input = document.querySelector("#search-input");
+  input.addEventListener("input", function (e) {
+    const text = e.target.value.toLocaleLowerCase().trim();
+    console.log("text", text);
+    const filteredData = data.filter((row) =>
+      Object.values(row).toLocaleString().toLocaleLowerCase().includes(text)
+    );
+    console.log("filtered data", filteredData);
+    store.array = filteredData;
+  });
 });
